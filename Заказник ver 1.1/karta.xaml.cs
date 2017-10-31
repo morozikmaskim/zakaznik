331030df8cdeff525170403c3fa8d20002e54b24
Максим <>
2017-10-31 06:28:48 +0300

    Добавьте файлы проекта.

---
+++ Заказник ver 1.1/karta.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Заказник_ver_1._1
+{
+    /// <summary>
+    /// Логика взаимодействия для karta.xaml
+    /// </summary>
+    public partial class karta : Window
+    {
+        public karta()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow form = new MainWindow();
+            form.Show();
+            this.Hide();
+            Close();
+        }
+
+        private void Button_nazad(object sender, RoutedEventArgs e)
+        {
+            Window1 form2 = new Window1();
+            form2.Show();
+            this.Hide();
+            Close();
+        }
+    }
+}
---
+++ Заказник ver 1.1/menu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Заказник_ver_1._1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow form = new MainWindow();
+            form.Show();
+            this.Hide();
+            Close();
+        }
+
+        private void MenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Карта_Click(object sender, RoutedEventArgs e)
+        {
+            karta karta = new karta();
+            karta.Show();
+            this.Hide();
+            Close();
+        }
+    }
+}
